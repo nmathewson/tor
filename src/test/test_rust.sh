5b43227de8053a08a067afb181220af7bc2a568f
Isis Lovecruft <isis@torproject.org>
2018-03-20 20:46:19 +0000

    tests: Automatically detect Rust crates to test and also pass --verbose.
     * FIXES #25560: https://bugs.torproject.org/25560.

--- src/test/test_rust.sh
+++ src/test/test_rust.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 # Test all Rust crates
 
-crates="protover tor_util smartlist tor_allocate"
+set -e
+
+manifests=`find ${abs_top_srcdir:-.}/src/rust/ -mindepth 2 -maxdepth 2 -name 'Cargo.toml'`
 
 exitcode=0
 
-set -e
-
-for crate in $crates; do
+for manifest in $manifests; do
     cd "${abs_top_builddir:-../../..}/src/rust"
     CARGO_TARGET_DIR="${abs_top_builddir:-../../..}/src/rust/target" \
       CARGO_HOME="${abs_top_builddir:-../../..}/src/rust" \
       "${CARGO:-cargo}" test --all-features ${CARGO_ONLINE-"--frozen"} \
-      --manifest-path "${abs_top_srcdir:-.}/src/rust/${crate}/Cargo.toml" \
+      --manifest-path "${manifest}" \
 	|| exitcode=1
     cd -
 done
