1a3fbf569b7a6df26fabfbe77c8d3ca778b793fd
Nick Mathewson <nickm@torproject.org>
2018-11-14 10:54:35 -0500

    Rough draft: use a ring buffer instead of a pair of stacks for mqueue
    (alternative implementation, using "first" and "last" indices instead of first
    and length)

--- src/lib/container/mqueue.c
+++ src/lib/container/mqueue.c
@@ -6,28 +6,56 @@
 /**
  * \file mqueue.c
  *
- * \brief Message-queue structure based on a pair of dynamic arrays.
+ * \brief Message-queue structure based on a ring buffer.
  **/
 
 #include "orconfig.h"
 #include "lib/malloc/malloc.h"
 #include "lib/container/mqueue.h"
 #include "lib/container/smartlist.h"
+#include "lib/log/util_bug.h"
+
+#include <string.h>
+#include <stdio.h>
+
+#define INITIAL_SIZE 16
 
 /** Initialize a mqueue_t structure. */
 void
 mqueue_init(mqueue_t *mq)
 {
-  mq->flushing = smartlist_new();
-  mq->filling = smartlist_new();
+  memset(mq, 0, sizeof(*mq));
+  mq->capacity = INITIAL_SIZE;
+  mq->head = 0;
+  mq->tail = 0;
+  mq->members = tor_calloc(INITIAL_SIZE, sizeof(void*));
 }
 
 /** Clear a mqueue_t structure.  Does not free the items held in the queue. */
 void
 mqueue_clear(mqueue_t *mq)
 {
-  smartlist_free(mq->flushing);
-  smartlist_free(mq->filling);
+  tor_free(mq->members);
+  memset(mq, 0, sizeof(*mq));
+}
+
+/** Return true iff mq has no elements. */
+static inline bool
+mqueue_empty(const mqueue_t *mq)
+{
+  return mq->head == mq->tail;
+}
+
+/** Return the next index in <b>mq</b> after <b>idx</b>, wrapping around
+ * if necessary */
+static inline size_t
+next_idx(const mqueue_t *mq, size_t idx)
+{
+  ++idx;
+  if (idx == mq->capacity)
+    return 0;
+  else
+    return idx;
 }
 
 /** Run <b>fn</b> on every element of <b>mq</b>, passing it <b>arg</b> as a
@@ -38,27 +66,56 @@ mqueue_clear(mqueue_t *mq)
 void
 mqueue_foreach(mqueue_t *mq, void (*fn)(void *, void *), void *userarg)
 {
-  for (int i = smartlist_len(mq->flushing)-1; i >= 0; --i) {
-    void *item = smartlist_get(mq->flushing, i);
-    fn(item, userarg);
+  for (size_t idx = mq->head; idx != mq->tail; idx = next_idx(mq, idx)) {
+    fn(mq->members[idx], userarg);
   }
-  SMARTLIST_FOREACH(mq->filling, void *, item, fn(item, userarg));
 }
 
 /** Return the number of elements stored in <b>mq</b>. */
 size_t
 mqueue_len(const mqueue_t *mq)
 {
-  size_t n = smartlist_len(mq->flushing);
-  n += smartlist_len(mq->filling);
-  return n;
+  if (mq->head <= mq->tail)
+    return mq->tail - mq->head;
+  else
+    return (mq->capacity - mq->head) + mq->tail;
+}
+
+static void
+mqueue_expand(mqueue_t *mq)
+{
+  size_t new_capacity = (mq->capacity * 2);
+
+  // Overflow should be impossible, but let's make sure.
+  tor_assert(new_capacity > mq->capacity);
+
+  void **new_members =
+    tor_reallocarray(mq->members, new_capacity, sizeof(void*));
+
+  if (mq->tail < mq->head) {
+    /* The ring buffer was wrapped around the end; we need to
+     * move the elements that were at the end of the array.
+     */
+    size_t delta = (new_capacity - mq->capacity); // == capacity
+    memmove(&new_members[mq->head + delta],
+            &new_members[mq->head],
+            (mq->capacity - mq->head) * sizeof(void*));
+    mq->head += delta;
+  }
+  mq->members = new_members;
+  mq->capacity = new_capacity;
 }
 
 /** Append <b>item</b> to the end of <b>mq</b>. */
 void
 mqueue_push(mqueue_t *mq, void *item)
 {
-  smartlist_add(mq->filling, item);
+  if (next_idx(mq, mq->tail) == mq->head) {
+    mqueue_expand(mq);
+  }
+
+  mq->members[mq->tail] = item;
+  mq->tail = next_idx(mq, mq->tail);
 }
 
 /** Remove and return the first item in <b>mq</b>.  Return NULL if <b>mq</b>
@@ -66,12 +123,12 @@ mqueue_push(mqueue_t *mq, void *item)
 void *
 mqueue_pop(mqueue_t *mq)
 {
-  if (smartlist_len(mq->flushing) == 0) {
-    smartlist_t *tmp = mq->flushing;
-    mq->flushing = mq->filling;
-    mq->filling = tmp;
-    smartlist_reverse(mq->flushing);
-  }
+  if (mq->head == mq->tail)
+    return NULL;
 
-  return smartlist_pop_last(mq->flushing);
+  void *result = mq->members[mq->head];
+  mq->members[mq->head] = NULL; // Clear the pointer, for safety.
+  mq->head = next_idx(mq, mq->head);
+
+  return result;
 }
--- src/lib/container/mqueue.h
+++ src/lib/container/mqueue.h
@@ -16,15 +16,18 @@
 
 struct smartlist_t;
 /**
- * A message queue, backed by a pair of smartlists.
+ * A message queue, implemented as a ring buffer.
  */
 typedef struct mqueue_t {
-  /** A list of items near the front of the queue -- they are stored
-   * in reverse order, so we can pop them off the end of the array. */
-  struct smartlist_t *flushing;
-  /** A list of items near the end of the queue -- they are stored in
-   * normal order, so we can push onto the end of the array. */
-  struct smartlist_t *filling;
+  /** A ring buffer of size <b>capacity</b>. */
+  void **members;
+  /** The amount of items that can be stored in <b>members</b>. */
+  size_t capacity;
+  /** The index of the head of the queue within members. */
+  size_t head;
+  /** The index one past the last item of the queue within members; if this is
+   * equal to "first" then the array is empty. */
+  size_t tail;
 } mqueue_t;
 
 void mqueue_init(mqueue_t *mq);
